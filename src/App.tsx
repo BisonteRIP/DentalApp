eb0a84f15ba6991bebb40d95e69a4f2a0a995c5d
Vicente Quiroz <99621vicenteaqv@gmail.com>
2026-02-28 23:32:44 -0400

    creacion de paginas e integracion de react route con sus rutas dentro de la carpeta /page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import "./App.css";
-import Header from "./components/Header";
 import { SidebarProvider } from "./components/ui/sidebar";
-import AppSidebar from "./components/App-sidebar";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+
+import Dashboard from "./pages/Dashboard";
+import Historial from "./pages/Historial";
+import Odontograma from "./pages/Odontograma";
+import Agenda from "./pages/Agenda";
 
 function App() {
   return (
     <>
       <SidebarProvider>
-        <AppSidebar />
-        <Header />
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/Dashboard" element={<Dashboard />} />
+            <Route path="/Odontograma" element={<Odontograma />} />
+            <Route path="/Historial" element={<Historial />} />
+            <Route path="/Agenda" element={<Agenda />} />
+          </Route>
+        </Routes>
       </SidebarProvider>
     </>
   );
